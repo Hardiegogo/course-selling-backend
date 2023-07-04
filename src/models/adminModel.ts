b9c2e7c2c284ed4aafe9661a98bf5edccb24f76a
Chirag Gupta <chirag.gps@gmail.com>
2023-07-04 17:39:26 +0530

    course-selling backend first draft

---
+++ src/models/adminModel.ts
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+interface IAdmin {
+  username: string;
+  password: string;
+}
+
+const AdminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const Admin = mongoose.model("Admin", AdminSchema);
+
+module.exports = Admin
